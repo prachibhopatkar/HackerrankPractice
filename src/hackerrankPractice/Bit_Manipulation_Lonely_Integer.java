dbb953e36ea9a09548d653f9dec4ed14a06882d4
prachibhopatkar <bhopatkarprachi@gmail.com>
2017-03-05 19:39:55 -0500

    bit manipulation

---
+++ src/hackerrankPractice/Bit_Manipulation_Lonely_Integer.java
@@ -0,0 +1,24 @@
+package hackerrankPractice;
+import java.util.Scanner;
+
+
+
+public class Bit_Manipulation_Lonely_Integer {
+public static int lonelyInteger(int[] a) {
+		int sum =0;
+        for(int i=0 ;i<a.length;i++){
+        	sum = sum ^ a[i];
+        }
+        return sum;
+    }
+    
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int a[] = new int[n];
+        for(int a_i=0; a_i < n; a_i++){
+            a[a_i] = in.nextInt();
+        }
+        System.out.println(lonelyInteger(a));
+    }
+}
